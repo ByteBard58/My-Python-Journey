35e15181387ff2f5d06d8dbb275ced8da6bab700
Sakib Hossain <sakibhossainalif29@gmail.com>
2025-04-12 10:28:21 +0600

    Update req4.auth.py

--- ChatGPT Roadmap/Phase -2/Request Module/req4.auth.py
+++ ChatGPT Roadmap/Phase -2/Request Module/req4.auth.py
@@ -4,3 +4,5 @@ from bs4 import BeautifulSoup as bsoup
 url ="https://httpbin.org/basic-auth/{user}/{passwd}"
 x = requests.get(url, auth=("Sakib","1348"))
 print(x)
+
+# That passowrd is just a demo, nothing serious 
